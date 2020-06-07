09f0e1610c7e37a67a30e10d1fa21290e457eaaa
pavelsumanoo <sumanoop@gmail.com>
2020-06-06 23:35:04 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null
+    };
+  }
+
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button className='square' onClick={() => this.setState({ value: 'X' })}>
+        {this.state.value}
       </button>
     );
   }
@@ -15,7 +21,7 @@ class Square extends React.Component {
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
@@ -23,18 +29,18 @@ class Board extends React.Component {
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
+        <div className='status'>{status}</div>
+        <div className='board-row'>
           {this.renderSquare(0)}
           {this.renderSquare(1)}
           {this.renderSquare(2)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(3)}
           {this.renderSquare(4)}
           {this.renderSquare(5)}
         </div>
-        <div className="board-row">
+        <div className='board-row'>
           {this.renderSquare(6)}
           {this.renderSquare(7)}
           {this.renderSquare(8)}
@@ -47,11 +53,11 @@ class Board extends React.Component {
 class Game extends React.Component {
   render() {
     return (
-      <div className="game">
-        <div className="game-board">
+      <div className='game'>
+        <div className='game-board'>
           <Board />
         </div>
-        <div className="game-info">
+        <div className='game-info'>
           <div>{/* status */}</div>
           <ol>{/* TODO */}</ol>
         </div>
@@ -62,8 +68,4 @@ class Game extends React.Component {
 
 // ========================================
 
-ReactDOM.render(
-  <Game />,
-  document.getElementById('root')
-);
-
+ReactDOM.render(<Game />, document.getElementById('root'));
